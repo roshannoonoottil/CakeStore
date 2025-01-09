d88a7c0c2744cb1ed3e42c2e20eb550d058ff22b
roshannoonoottil <roshannoonoottil@gmail.com>
2025-01-09 16:21:35 +0530

    cakeshop 1st commit

--- src/Components/CakeShop.jsx
+++ src/Components/CakeShop.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { buyCake } from '../redux/cake/cakeActions'
 
 function CakeShop() {
+    const numOfCakes = useSelector((state)=>state.numOfCakes)
+    const dispatch = useDispatch()
   return (
     <div>
       <h1>Number Of cakes</h1>
-      <button>Buy Cake</button>
+      <h2>{numOfCakes}</h2>
+      <button onClick={()=>dispatch(buyCake())}>Buy Cake</button>
     </div>
   )
 }
