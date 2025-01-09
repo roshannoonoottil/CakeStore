826e16e9c34a34c6f45424c0307ef94e35dc3e21
roshannoonoottil <roshannoonoottil@gmail.com>
2025-01-09 12:40:50 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import CakeShop from './Components/CakeShop'
+
+function App() {
+
+  return (
+<>
+<CakeShop/>
+</>
+  )
+}
+
+export default App
---
+++ src/Components/CakeShop.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CakeShop() {
+  return (
+    <div>
+      <h1>Number Of cakes</h1>
+      <button>Buy Cake</button>
+    </div>
+  )
+}
+
+export default CakeShop
