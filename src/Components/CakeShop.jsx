20b3cfeb3c778225a5b2427fc90a1dca49e94a18
roshannoonoottil <roshannoonoottil@gmail.com>
2025-01-11 09:54:14 +0530

    Action payload added

--- src/Components/CakeShop.jsx
+++ src/Components/CakeShop.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { buyCake } from '../redux/cake/cakeActions'
 import { buyIcecream } from '../redux/icecream/icescreamActions'
@@ -6,13 +6,15 @@ import { buyIcecream } from '../redux/icecream/icescreamActions'
 function CakeShop() {
     const numOfCakes = useSelector((state)=>state.cake.numOfCakes)
     const numOfIceCream = useSelector((state)=>state.icecream.numOfIceCream)
+    const [number, setNumber] = useState(1)
 
     const dispatch = useDispatch()
   return (
     <div>
       <h1>Number Of cakes</h1>
       <h2>{numOfCakes}</h2>
-      <button onClick={()=>dispatch(buyCake())}>Buy Cake</button>
+      <input type="text" value={number} onChange={e => setNumber(e.target.value)} />
+      <button onClick={()=>dispatch(buyCake(number))}>Buy {number} Cake</button>
 
       <h1>Number Of IceCreams</h1>
       <h2>{numOfIceCream}</h2>
